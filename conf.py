8cb1df0af5afe4060f1cef2f60dd5c9413e19853
Kozo Nishida <knishida@riken.jp>
2013-06-14 18:52:14 +0900

    add conf.py

---
+++ conf.py
@@ -0,0 +1,25 @@
+import sys, os
+
+extensions = []
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = u'ecell3'
+copyright = u'2013, ecell project'
+
+version = '3.2.2'
+release = '3.2.2'
+
+exclude_trees = ['_build']
+
+pygments_style = 'sphinx'
+
+html_theme = 'default'
+
+html_static_path = ['_static']
+
+htmlhelp_basename = 'ecell3-doc'
